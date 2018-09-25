26feeb0d2fbce4db530b4dd84f77953d86642ff9
Eugene Sadovoi <evgeni@eniks.com>
2018-09-24 20:52:56 -0400

    Moving BuildContext to be a structure

--- src/Log4NetExtension.cs
+++ src/Log4NetExtension.cs
@@ -5,7 +5,7 @@ using Unity.Policy;
 
 namespace Unity.log4net
 {
-    public class Log4NetExtension : UnityContainerExtension, IBuildPlanPolicy
+    public class Log4NetExtension : UnityContainerExtension
     {
         private static readonly Func<Type, string, string> _defaultGetName = (t, n) => t.FullName;
 
@@ -13,20 +13,20 @@ namespace Unity.log4net
 
         protected override void Initialize()
         {
-            Context.Policies.Set(typeof(ILog), null, typeof(IBuildPlanPolicy), this);
+            // TODO: Context.Policies.Set(typeof(ILog), null, typeof(IBuildPlanPolicy), this);
         }
 
-        void IBuildPlanPolicy.BuildUp<TContext>(ref TContext context)
-        {
-            Func<Type, string, string> method = GetName ?? _defaultGetName;
-#if NETSTANDARD1_3
-            context.Existing = LogManager.GetLogger(context.ParentContext?.BuildKey.Type);
-#else
-            context.Existing = LogManager.GetLogger(method(context.ParentContext?.BuildKey.Type,
-                                                           context.ParentContext?.BuildKey.Name));
-#endif
-            context.BuildComplete = true;
-        }
+//        void IBuildPlanPolicy.BuildUp<TContext>(ref TContext context)
+//        {
+//            Func<Type, string, string> method = GetName ?? _defaultGetName;
+//#if NETSTANDARD1_3
+//            context.Existing = LogManager.GetLogger(context.ParentContext?.BuildKey.Type);
+//#else
+//            context.Existing = LogManager.GetLogger(method(context.ParentContext?.BuildKey.Type,
+//                                                           context.ParentContext?.BuildKey.Name));
+//#endif
+//            context.BuildComplete = true;
+//        }
 
     }
 }
